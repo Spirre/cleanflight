c06769f8e792072b4d61a9a1c7ee7a402d6a25ad
Michael Jakob <michael.p.jakob@web.de>
2015-05-08 17:00:27 +0200

    Winbond W25Q128 SPI flash support (16Mbyte)

--- src/main/drivers/flash_m25p16.c
+++ src/main/drivers/flash_m25p16.c
@@ -42,6 +42,7 @@
 #define JEDEC_ID_MICRON_M25P16         0x202015
 #define JEDEC_ID_MICRON_N25Q064        0x20BA17
 #define JEDEC_ID_WINBOND_W25Q64        0xEF4017
+#define JEDEC_ID_WINBOND_W25Q128       0xEF4018
 
 #define DISABLE_M25P16       GPIO_SetBits(M25P16_CS_GPIO,   M25P16_CS_PIN)
 #define ENABLE_M25P16        GPIO_ResetBits(M25P16_CS_GPIO, M25P16_CS_PIN)
@@ -157,6 +158,11 @@ static bool m25p16_readIdentification()
         case JEDEC_ID_MICRON_N25Q064:
         case JEDEC_ID_WINBOND_W25Q64:
             geometry.sectors = 128;
+            geometry.pagesPerSector = 256;
+            geometry.pageSize = 256;
+        break;
+        case JEDEC_ID_WINBOND_W25Q128:
+            geometry.sectors = 256;
             geometry.pagesPerSector = 256;
             geometry.pageSize = 256;
         break;
